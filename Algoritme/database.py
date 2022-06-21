9970d8f0a28906b3df61f21b07fc65c228275357
Bandaan <rensdaan2002@gmail.com>
2022-06-21 23:06:34 +0200

    Algoritme afgemaakt + Product page toegevoegd (Daan)

---
+++ Algoritme/database.py
@@ -0,0 +1,78 @@
+import psycopg2
+import time
+import os
+
+
+class Database:
+    def __init__(self):
+        try:
+            # Connectie maken met de sql database
+            self.conn = psycopg2.connect(
+                host='localhost',
+                dbname='supermarkt',
+                user='postgres',
+                password='AjaxDaan23@',
+                port='5432'
+            )
+            self.cur = self.conn.cursor()
+
+        # Error met het connecten van een van de databases
+        except Exception as error:
+
+            # De error laten zien aan de client
+            print(f'Unable to connect to the database {error}')
+            time.sleep(5)
+
+            # Het programma stoppen
+            os._exit(0)
+
+    def main(self):
+        self.delete_tables()
+        self.create_table()
+
+    def delete_tables(self):
+        # Functie om alle tabellen te verwijderen
+
+        # Kijken welke tabellen er allemaal in de database zitten
+        self.cur.execute(f"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='webshop';")
+
+        # Alle tabellen die niet weg mogen eruit filteren
+        tables = []
+        for result in self.cur:
+            if 'pg' in str(result[0]) or 'sql' in str(result[0]) or str(result[0]) in ["product"]:
+                pass
+            else:
+                tables.append(result[0])
+
+        # Gefilterde tabellen verwijderen uit database
+        for table in tables:
+            self.cur.execute(f"DROP table {table} CASCADE;")
+
+        self.cur.execute("drop table product;")
+
+        # Definitief in database zetten
+        self.conn.commit()
+
+        return
+
+    def create_table(self):
+        self.cur.execute("CREATE TABLE Product (pid bigint NOT NULL,productName varchar(255),productLink varchar(255), productImage varchar(255), productPrice varchar(255), productWeight varchar(255),energie varchar(255),vet varchar(255),koolhydraten varchar(255),eiwitten varchar(255),zout varchar(255),voedingsvezel varchar(255), PRIMARY KEY (pid));")
+
+        self.conn.commit()
+        pass
+
+    async def insert_table(self, product_info):
+
+        insert_script = 'INSERT INTO Product (pid, productName, productLink, productImage, productPrice, productWeight, energie, vet, koolhydraten, eiwitten, zout, voedingsvezel) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        insert_value = (product_info['product_pid'], product_info['product_name'], product_info['product_link'], product_info['product_image'], product_info['product_price'], product_info['product_weight'], product_info['Energie'], product_info['Vet'], product_info['Koolhydraten'], product_info['Eiwitten'], product_info['Zout'], product_info['Voedingsvezel'],)
+
+        self.cur.execute(insert_script, insert_value)
+
+    def first10_products(self):
+        self.cur.execute("select * from product limit 100;")
+
+        return self.cur.fetchall()
+
+    async def commit(self):
+        self.conn.commit()
+
---
+++ Algoritme/productdatabase.py
@@ -0,0 +1,178 @@
+import requests
+import asyncio
+import aiohttp
+import database
+import json
+import time
+import os
+
+
+class Product:
+    def __init__(self):
+        self.categories = ["aardappel-groente-fruit", "salades-pizza-maaltijden", "vlees-kip-vis-vega", "kaas-vleeswaren-tapas", "zuivel-plantaardig-en-eieren", "bakkerij-en-banket", "ontbijtgranen-en-beleg",
+                            "snoep-koek-chips-en-chocolade", "tussendoortjes", "frisdrank-sappen-koffie-thee", "wijn-en-bubbels", "bier-en-aperitieven", "pasta-rijst-en-wereldkeuken", "soepen-sauzen-kruiden-olie",
+                            "sport-en-dieetvoeding", "diepvries", "drogisterij", "baby-en-kind", "huishouden", "huisdier", "koken-tafelen-vrije-tijd"]
+
+        #self.categories = ["salades-pizza-maaltijden"]
+        self.products = []
+        self.product_pids = []
+        self.tasks = []
+        self.error_pids = []
+        self.test = []
+
+    async def main(self):
+
+        for cat in self.categories:
+            pids = await self.get_product_pids(cat)
+
+            for j in pids:
+                self.product_pids.append(j)
+
+        self.product_pids = list(dict.fromkeys(self.product_pids))
+
+        index = 0
+        for i in self.product_pids:
+            self.tasks.append(asyncio.create_task(self.get_product_info(i, index)))
+            index += 1
+
+        await asyncio.gather(*self.tasks)
+
+        await db.commit()
+
+    async def get_product_pids(self, categorie):
+
+        req = requests.get(f"https://www.ah.nl/zoeken/api/products/search?taxonomySlug={categorie}&size=1000")
+
+        product_ids = []
+        products = req.json()['cards']
+
+        for i in range(len(products)):
+            if products[i]['id'] not in product_ids:
+                product_ids.append(products[i]['id'])
+
+        if int(req.json()["page"]["totalElements"]) > 1000:
+            elements = int(req.json()["page"]["totalElements"])
+
+            links = [f"https://www.ah.nl/zoeken/api/products/search?page=1&taxonomySlug={categorie}&size=1000"]
+
+            elements -= 1000
+            index = 2
+            while elements != 0:
+                if elements >= 1000:
+                    links.append(f"https://www.ah.nl/zoeken/api/products/search?page={index}&taxonomySlug={categorie}&size=1000")
+                    elements -= 1000
+                    index += 1
+                else:
+                    links.append(f"https://www.ah.nl/zoeken/api/products/search?page={index}&taxonomySlug={categorie}&size={elements}")
+                    elements = 0
+
+            for endpoint in links:
+                req = requests.get(endpoint)
+
+                try:
+                    products = req.json()['cards']
+                except Exception:
+                    pass
+
+                for j in range(len(products)):
+                    if products[j]['id'] not in product_ids:
+                        product_ids.append(products[j]['id'])
+
+        return product_ids
+
+    async def get_product_info(self, pid, index):
+        try:
+            async with aiohttp.ClientSession() as session:
+                error = 0
+
+                while True:
+                    try:
+                        result = await session.get(f'https://www.ah.nl/zoeken/api/products/product?webshopId={pid}')
+                    except Exception:
+                        if error > 2:
+                            self.error_pids.append(pid)
+                            return
+                        error += 1
+                        continue
+                    else:
+                        if result.status == 200:
+                            break
+                        else:
+                            print(await result.json(content_type=None))
+                            return
+
+                response = await result.json(content_type=None)
+
+                product_info = {}
+
+                try:
+                    voedingsstoffen = len(response['card']['meta']['nutritions'][0]['nutrients'])
+                except KeyError:
+                    voedingsstoffen = 0
+
+                for p in range(voedingsstoffen):
+                    product_info[response['card']['meta']['nutritions'][0]['nutrients'][p]['name']] = response['card']['meta']['nutritions'][0]['nutrients'][p]['value']
+
+                try:
+                    product_info['product_image'] = response['card']['products'][0]['images'][3]['url']
+                except Exception:
+                    product_info['product_image'] = 'https://upload.wikimedia.org/wikipedia/commons/e/eb/Albert_Heijn_Logo.svg'
+
+                try:
+                    product_info['product_name'] = response['card']['products'][0]['title']
+                    product_info['product_link'] = response['card']['products'][0]['link']
+                    product_info['product_price'] = response['card']['products'][0]['price']['now']
+                    product_info['product_weight'] = response['card']['products'][0]['price']['unitSize']
+
+                except KeyError:
+                    product_info['product_name'] = 0
+                    product_info['product_link'] = 0
+                    product_info['product_price'] = 0
+                    product_info['product_weight'] = 0
+
+            product_info['product_pid'] = pid
+
+            if not product_info.get("Energie"):
+                product_info["Energie"] = 0
+            if not product_info.get("Vet"):
+                product_info["Vet"] = 0
+            if not product_info.get("Koolhydraten"):
+                product_info["Koolhydraten"] = 0
+            if not product_info.get("Eiwitten"):
+                product_info["Eiwitten"] = 0
+            if not product_info.get("Zout"):
+                product_info["Zout"] = 0
+            if not product_info.get("Voedingsvezel"):
+                product_info["Voedingsvezel"] = 0
+
+            if "kg" in str(product_info.get("product_weight")):
+                print(product_info["product_pid"])
+                s_nums = "".join([ch for ch in str(product_info.get("product_weight")) if ch.isdigit()])
+                product_info["product_weight"] = f"{int(s_nums) * 1000} g"
+
+            if "stuk" in str(product_info.get("product_weight")) or "per" in str(product_info.get("product_weight")):
+                if response['card'].get("meta"):
+                    try:
+                        gewicht = response['card']['meta']['contents']['netContents'][0]
+
+                    except Exception:
+                        gewicht = 0
+                        pass
+
+                    s_nums = "".join([ch for ch in str(gewicht) if ch.isdigit()])
+                    product_info["product_weight"] = f"{int(s_nums)} g"
+
+            await db.insert_table(product_info)
+            await db.commit()
+
+            return
+
+        except asyncio.TimeoutError:
+            self.error_pids.append(pid)
+            print( {"results": f"timeout error on {pid}"})
+
+
+if __name__ == "__main__":
+    db = database.Database()
+    db.main()
+    asyncio.get_event_loop().run_until_complete(Product().main())
